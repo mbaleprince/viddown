546eafa9efcd1ab4c6e81e43ba9f5f550231ad87
mbale prince <mbaleprince29@gmail.com>
2026-04-02 12:43:40 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def fetch_video():
         return jsonify({'error': 'Failed to process video: ' + str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=6000)
